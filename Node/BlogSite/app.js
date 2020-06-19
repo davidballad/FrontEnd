188762cc76f045c4e6ae2487218a889501e92554
davidballad <davicho_balla@hotmail.com>
2020-06-19 14:32:18 -0500

    add mongdb to blogme app

--- Node/BlogSite/app.js
+++ Node/BlogSite/app.js
@@ -8,13 +8,29 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname+'/public'));
 const _ = require('lodash');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blogsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const blogs = [{blogTitle:'hi there', blogContent: 'Integer porta dictum sagittis. Etiam mattis et velit a dapibus. Proin euismod lacus augue, a ornare lacus pellentesque sit amet. Aliquam id ipsum posuere, volutpat dui vel, accumsan urna. Ut feugiat orci sed quam facilisis vulputate dignissim nec orci. Sed viverra mauris magna, vestibulum ullamcorper mauris vehicula quis. Suspendisse potenti. Morbi magna lectus, pellentesque in diam sit amet, molestie scelerisque urna. Praesent et felis ac risus tristique vehicula. Quisque egestas at massa sit amet porta. Morbi bibendum blandit nisl a consectetur. Etiam in consequat ipsum. Sed in congue nisl. Etiam consequat, velit in varius iaculis, lorem nunc luctus eros, vel aliquet leo neque at leo. '}];
+const blogSchema = new mongoose.Schema({
+      blogTitle: String,
+      blogContent: String
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+//const blogs = [{blogTitle:'hi there', blogContent: 'Integer porta dictum sagittis. Etiam mattis et velit a dapibus. Proin euismod lacus augue, a ornare lacus pellentesque sit amet. Aliquam id ipsum posuere, volutpat dui vel, accumsan urna. Ut feugiat orci sed quam facilisis vulputate dignissim nec orci. Sed viverra mauris magna, vestibulum ullamcorper mauris vehicula quis. Suspendisse potenti. Morbi magna lectus, pellentesque in diam sit amet, molestie scelerisque urna. Praesent et felis ac risus tristique vehicula. Quisque egestas at massa sit amet porta. Morbi bibendum blandit nisl a consectetur. Etiam in consequat ipsum. Sed in congue nisl. Etiam consequat, velit in varius iaculis, lorem nunc luctus eros, vel aliquet leo neque at leo. '}];
 app.get('/', (req, res)=>{
-   res.render('home', {
-      title: 'Blog Site',
-      blogs: blogs
+   Blog.find({}, (err, blogs)=>{
+      
+      const content = blogs;
+
+      res.render('home', {
+         title: 'Blog Site',
+         blogs: content
+      });
    });
+
+   
 });
 
 
@@ -25,33 +41,43 @@ app.get('/createBlog', (req, res)=>{
 });
 
 app.post('/', (req, res)=>{
-   const blog = {
+   const blog = new Blog({
       blogContent: req.body.blogContent,
       blogTitle: req.body.blogTitle
-   };
-   blogs.push(blog);
+   });
+   blog.save();
    res.redirect('/');
 
 });
 
 
-app.get('/blog/:title', (req, res)=>{
-   const reqTitle = _.kebabCase(req.params.title);
-   req.params.title = reqTitle;
-      blogs.forEach((blog)=>{
-         const listTitle = _.kebabCase(blog.blogTitle); 
-         if(listTitle === reqTitle){
-            const listContent = blog.blogContent;
-            
-            console.log(req.params.title);
-            res.render('blog',{
-               title: blog.blogTitle,
-               blogContent: listContent,
-               blogTitle: blog.blogTitle
-            });
-         } 
-
+app.get('/blog/:id', (req, res)=>{
+   //const reqTitle = _.kebabCase(req.params.title);
+   const reqId = req.params.id;
+   req.params.id = reqId;
+   Blog.findOne({_id: reqId}, (err, blog)=>{
+      const listContent = blog.blogContent;
+      console.log(req.params.id);
+      res.render('blog',{
+         title: blog.blogTitle,
+         blogContent: listContent,
+         blogTitle: blog.blogTitle
       });
+   });
+      // blogs.forEach((blog)=>{
+      //    const listTitle = _.kebabCase(blog.blogTitle); 
+      //    if(listTitle === reqTitle){
+      //       const listContent = blog.blogContent;
+            
+      //       console.log(req.params.title);
+      //       res.render('blog',{
+      //          title: blog.blogTitle,
+      //          blogContent: listContent,
+      //          blogTitle: blog.blogTitle
+      //       });
+      //    } 
+
+      // });
      
       
 });
